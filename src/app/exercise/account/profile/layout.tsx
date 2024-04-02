5e8243d8fb15405d7b7bf2e7720755da48d0770d
Mike Codeur <cabanas.mickael@gmail.com>
2024-04-02 15:09:35 +0800

    prepare exo

--- src/app/exercise/account/profile/layout.tsx
+++ src/app/exercise/account/profile/layout.tsx
@@ -1,3 +1,10 @@
-//export {default} from './layout.bonus-1'
+import React from 'react'
 
-export {default} from './layout.exercise'
+export default function Layout({children}: {children: React.ReactNode}) {
+  return (
+    <div className="flex h-[200px] w-[400px] flex-col items-center justify-center border-4 border-yellow-700 bg-yellow-500 font-bold text-white">
+      <h2>Zone Danger</h2>
+      {children}
+    </div>
+  )
+}
