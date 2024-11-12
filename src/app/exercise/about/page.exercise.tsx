54fb6aaa6203f52e855ea56e1abc52c7a3e0c85c
tloyan <thomas.loyan@gmail.com>
2024-11-12 12:41:48 +0100

    feature/routes: complete exercice

--- src/app/exercise/about/page.exercise.tsx
+++ src/app/exercise/about/page.exercise.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 const Page = () => {
   // 🐶 Détermine dans quel environnement s'exécute ce composant grâce à un log
   // 🤖 `console.log('Je suis un développeur FullStack Next')`
@@ -5,8 +7,9 @@ const Page = () => {
   // 🐶 Vérifie dans la console du navigateur et dans la console du serveur (terminal)
 
   // 🐶 N'oublie pas de faire les exercices bonus
+  console.log("Je suis un developpeur FullStack Next")
   return (
-    <div>
+    <div onClick={() => alert("Hello")}>
       <p>Je suis un développeur FullStack Next</p>
     </div>
   )
